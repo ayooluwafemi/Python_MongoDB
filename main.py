3179da67f5003ab5c32130b7a2d0f09ac312cc97
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-08 23:14:08 +0100

    MongoDB Limit

--- main.py
+++ main.py
@@ -136,8 +136,21 @@ print(x.deleted_count, " documents deleted.") """
 """ mycol.drop() """
 
 # MONGODB UPDATE
+# Change the address from "Valley 345" to "Canyon 123":
+""" myquery = { "address": "Valley 345" }
+newvalues = { "$set": { "address": "Canyon 123" } }
+mycol.update_one(myquery, newvalues)
+#print "customers" after the update:
+for x in mycol.find():
+  print(x) """
+  
+# Update all documents where the address starts with the letter "S":
+""" myquery = { "address": { "$regex": "^S" } }
+newvalues = { "$set": { "name": "Minnie" } }
+x = mycol.update_many(myquery, newvalues)
+print(x.modified_count, "documents updated.") """
 
-
+# MONGODB LIMIT
 
 
 
